e1156d91500ce4723ec875a5040e2ad07a9a266a
SANDRA\Sandy <dsandy_26@web.de>
2017-05-26 15:34:19 +0200

    Tacos verschwinden bei Kollision und erhöhen Punktestand um 1

---
+++ trump_n_run/Assets/scripts/Tacoeinsammeln.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tacoeinsammeln : MonoBehaviour {
+
+    public int tacoCounter; 
+
+
+    // Use this for initialization
+    void Start() {
+        tacoCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        {
+            tacoCounter++;
+            if (other.tag == "Taco")
+            {
+                Destroy(other.gameObject);
+                Debug.Log("Score: " +tacoCounter);
+            }
+
+
+        }
+    }
+}
