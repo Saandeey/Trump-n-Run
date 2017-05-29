a9753c005085240cd29e7b7b239667da8a0ce6cd
Katharina <k-siems@web.de>
2017-05-29 12:46:07 +0200

    Update

--- trump_n_run/Assets/Mexican/CharacterWalk.cs
+++ trump_n_run/Assets/Mexican/CharacterWalk.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CharacterWalk : MonoBehaviour
 {
 	public float moveSpeed;
+	public int jumpforce;
 
 	// Update is called once per frame
 	void Update()
@@ -12,6 +14,11 @@ public class CharacterWalk : MonoBehaviour
 	
 
 		transform.Translate(new Vector3(h, 0, 0) * Time.deltaTime * moveSpeed);
+
+		if (Input.GetKey ("Jump")){
+
+			GetComponent<Rigidbody2D>().AddForce (new Vector2 (0,1).normalized * jumpforce);
+		}
 	}
 
 
