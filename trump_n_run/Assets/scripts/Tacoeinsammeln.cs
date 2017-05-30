291aa0d51002a17eb828a5bdae5e25806202fa70
Katharina <k-siems@web.de>
2017-05-30 10:21:58 +0200

    update

--- trump_n_run/Assets/scripts/Tacoeinsammeln.cs
+++ trump_n_run/Assets/scripts/Tacoeinsammeln.cs
@@ -1,34 +1,36 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Tacoeinsammeln : MonoBehaviour {
+//public class Tacoeinsammeln : MonoBehaviour {
 
-    public int tacoCounter; 
+  //  public int tacoCounter; 
 
 
     // Use this for initialization
-    void Start() {
-        tacoCounter = 0;
-    }
+  //  void Start() {
+    //    tacoCounter = 0;
+    //}
 
     // Update is called once per frame
-    void Update() {
+    //void Update() {
 
-    }
+    //}
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        {
-            tacoCounter++;
-            if (other.tag == "Taco")
-            {
-                Destroy(other.gameObject);
-                Debug.Log("Score: " +tacoCounter);
-            }
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+        
+            
+      //      if (other.tag == "Taco")
+        //    {
+          //      Destroy(other.gameObjects);
+			//Debug.Log("Score: " +tacoCounter);
+			//tacoCounter++;
+
+            //}
 
 
-        }
-    }
-}
+        
+    //}
+//}
