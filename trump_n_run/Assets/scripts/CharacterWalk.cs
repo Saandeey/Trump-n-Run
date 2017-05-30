3b7c165767fbdf19354ee190ccd759c0023f99dc
Katharina <k-siems@web.de>
2017-05-30 11:39:04 +0200

    Player springt

--- trump_n_run/Assets/scripts/CharacterWalk.cs
+++ trump_n_run/Assets/scripts/CharacterWalk.cs
@@ -7,19 +7,28 @@ public class CharacterWalk : MonoBehaviour
 	public float moveSpeed;
 	public int jumpforce;
 
+
+
+
 	// Update is called once per frame
 	void Update()
 	{
 		float h = Input.GetAxis("Horizontal");
-	
+
 
 		transform.Translate(new Vector3(h, 0, 0) * Time.deltaTime * moveSpeed);
 
-		if (Input.GetKey ("Jump")){
+		//if (Input.GetButton ("Jump")){
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		//GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
+	
 
-			GetComponent<Rigidbody2D>().AddForce (new Vector2 (0,1).normalized * jumpforce);
+		
+			if (Input.GetKeyDown(KeyCode.UpArrow))
+				rb.AddForce(Vector2.up * jumpforce);
+
 		}
 	}
+	
 
 
-}
